91f0d80459dfc1af3c3f8fb6b2ed4957c3b1059d
Tom Petrangelo <48896889+tpetrangelo@users.noreply.github.com>
2026-02-16 21:58:39 +0000

    updated deploy.sh to rebuild docker image

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
 git pull
-docker compose -f docker/docker-compose.yaml restart airflow-scheduler airflow-webserver
+
+docker compose -f docker/docker-compose.yaml down
+
+docker compose -f docker/docker-compose.yaml build --no-cache
+
+docker compose -f docker/docker-compose.yaml up -d
